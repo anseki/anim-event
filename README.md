c154c3bffdc698b3c08f11ff5c7eb77a07eed002
anseki <AnSeki.aff@gmail.com>
2016-08-19 00:21:35 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# AnimEvent
+
+Super Simple Event Manager for Animation
+
+Some DOM events (e.g. `scroll`, `resize`, `mousemove`, `drag`, etc.) are fired too frequently.  
+The listening to those events for animation is performance degradation.  
+AnimEvent controls timing of calling event listeners with [`requestAnimationFrame`](https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame) (or 60fps in a web browser that doesn't support it).
+
+AnimEvent works like [lodash's `throttle` function](https://lodash.com/docs#throttle), but it uses `requestAnimationFrame` that is more optimized for animation, instead of "wait-time".
+
+## Usage
+
+Load AnimEvent into a web page.
+
+```html
+<script src="anim-event.min.js"></script>
+```
+
+Pass `AnimEvent.add(listener)` instead of `listener` to [`addEventListener` method](https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener).
+
+For example, replace first code with second code:
+
+```js
+window.addEventListener('scroll', listener, false);
+```
+
+```js
+window.addEventListener('scroll', AnimEvent.add(listener), false);
+```
+
+Then `listener` is called when the window is scrolled, with optimized timing for animation. Superfluous fired events are ignored.
+
+## Methods
+
+### `AnimEvent.add`
+
+```js
+wrappedListener = AnimEvent.add(listener)
+```
+
+Add an event listener that is controlled by AnimEvent.  
+Pass a returned `wrappedListener` to `addEventListener` method.
+
+A listener that has already been added can not be added.  
+For example, use one listener for multiple events:
+
+```js
+var listener = AnimEvent.add(function(event) { console.log(event); });
+window.addEventListener('scroll', listener, false);
+window.addEventListener('resize', listener, false);
+```
+
+### `AnimEvent.remove`
+
+```js
+AnimEvent.remove(listener)
+```
+
+Remove an event listener that was added by [`AnimEvent.add` method](#animeventadd).  
+You can remove a `wrappedListener` that was added by `addEventListener` method from the event by [`removeEventListener` method](https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/removeEventListener). `AnimEvent.remove` method removes a listener from listeners that are controlled by AnimEvent.
---
+++ anim-event.js
@@ -0,0 +1,105 @@
+/*
+ * AnimEvent
+ * https://github.com/anseki/anim-event
+ *
+ * Copyright (c) 2016 anseki
+ * Licensed under the MIT license.
+ */
+
+/* exported AnimEvent */
+
+;var AnimEvent = (function() { // eslint-disable-line no-extra-semi
+  'use strict';
+
+  var
+    MSPF = 1000 / 60, // ms/frame (FPS: 60)
+    KEEP_LOOP = 500,
+
+    requestAnim = window.requestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      function(callback) { setTimeout(callback, MSPF); },
+    cancelAnim = window.cancelAnimationFrame ||
+      window.mozCancelAnimationFrame ||
+      window.webkitCancelAnimationFrame ||
+      window.msCancelAnimationFrame ||
+      function(requestID) { clearTimeout(requestID); },
+
+    /**
+     * @typedef {Object} task
+     * @property {Event} event
+     * @property {function} listener
+     */
+
+    /** @type {task[]} */
+    tasks = [],
+    requestID, lastFrameTime = Date.now();
+
+  function step() {
+    var called, next;
+
+    if (requestID) {
+      cancelAnim.call(window, requestID);
+      requestID = null;
+    }
+
+    tasks.forEach(function(task) {
+      if (task.event) {
+        task.listener(task.event);
+        task.event = null;
+        called = true;
+      }
+    });
+
+    if (called) {
+      lastFrameTime = Date.now();
+      next = true;
+    } else if (Date.now() - lastFrameTime < KEEP_LOOP) { // Go on for a while
+      next = true;
+    }
+    if (next) { requestID = requestAnim.call(window, step); }
+  }
+
+  function indexOfTasks(listener) {
+    var index = -1;
+    tasks.some(function(task, i) {
+      if (task.listener === listener) {
+        index = i;
+        return true;
+      }
+      return false;
+    });
+    return index;
+  }
+
+  return {
+    /**
+     * @param {function} listener - An event listener.
+     * @returns {(function|null)} - A wrapped event listener.
+     */
+    add: function(listener) {
+      var task;
+      if (indexOfTasks(listener) === -1) {
+        tasks.push((task = {listener: listener}));
+        return function(event) {
+          task.event = event;
+          if (!requestID) { step(); }
+        };
+      } else {
+        return null;
+      }
+    },
+
+    remove: function(listener) {
+      var iRemove;
+      if ((iRemove = indexOfTasks(listener)) > -1) {
+        tasks.splice(iRemove, 1);
+        if (!tasks.length && requestID) {
+          cancelAnim.call(window, requestID);
+          requestID = null;
+        }
+      }
+    }
+  };
+})();
