7315d06de7409a3fca3b820a4a08dc68d9fdb4c6
anseki <AnSeki.aff@gmail.com>
2017-01-19 22:48:04 +0900

    rebuild with webpack v2.2

--- test/anim-event.js
+++ test/anim-event.js
@@ -12,16 +12,16 @@ var AnimEvent =
 /******/
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
-/******/ 			exports: {},
-/******/ 			id: moduleId,
-/******/ 			loaded: false
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
 /******/ 		};
 /******/
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 /******/
 /******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
+/******/ 		module.l = true;
 /******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
@@ -34,129 +34,156 @@ var AnimEvent =
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
 /******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-	"use strict";
-	
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	/*
-	 * AnimEvent
-	 * https://github.com/anseki/anim-event
-	 *
-	 * Copyright (c) 2016 anseki
-	 * Licensed under the MIT license.
-	 */
-	
-	// *** Currently, this code except `export` is not ES2015. ***
-	
-	var MSPF = 1000 / 60,
-	    // ms/frame (FPS: 60)
-	KEEP_LOOP = 500,
-	    requestAnim = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
-	  setTimeout(callback, MSPF);
-	},
-	    cancelAnim = window.cancelAnimationFrame || window.mozCancelAnimationFrame || window.webkitCancelAnimationFrame || window.msCancelAnimationFrame || function (requestID) {
-	  clearTimeout(requestID);
-	},
-	
-	
-	/**
-	 * @typedef {Object} task
-	 * @property {Event} event
-	 * @property {function} listener
-	 */
-	
-	/** @type {task[]} */
-	tasks = [],
-	    requestID,
-	    lastFrameTime = Date.now();
-	
-	function step() {
-	  var called, next;
-	
-	  if (requestID) {
-	    cancelAnim.call(window, requestID);
-	    requestID = null;
-	  }
-	
-	  tasks.forEach(function (task) {
-	    if (task.event) {
-	      task.listener(task.event);
-	      task.event = null;
-	      called = true;
-	    }
-	  });
-	
-	  if (called) {
-	    lastFrameTime = Date.now();
-	    next = true;
-	  } else if (Date.now() - lastFrameTime < KEEP_LOOP) {
-	    // Go on for a while
-	    next = true;
-	  }
-	  if (next) {
-	    requestID = requestAnim.call(window, step);
-	  }
-	}
-	
-	function indexOfTasks(listener) {
-	  var index = -1;
-	  tasks.some(function (task, i) {
-	    if (task.listener === listener) {
-	      index = i;
-	      return true;
-	    }
-	    return false;
-	  });
-	  return index;
-	}
-	
-	var AnimEvent = {
-	  /**
-	   * @param {function} listener - An event listener.
-	   * @returns {(function|null)} - A wrapped event listener.
-	   */
-	  add: function add(listener) {
-	    var task;
-	    if (indexOfTasks(listener) === -1) {
-	      tasks.push(task = { listener: listener });
-	      return function (event) {
-	        task.event = event;
-	        if (!requestID) {
-	          step();
-	        }
-	      };
-	    } else {
-	      return null;
-	    }
-	  },
-	
-	  remove: function remove(listener) {
-	    var iRemove;
-	    if ((iRemove = indexOfTasks(listener)) > -1) {
-	      tasks.splice(iRemove, 1);
-	      if (!tasks.length && requestID) {
-	        cancelAnim.call(window, requestID);
-	        requestID = null;
-	      }
-	    }
-	  }
-	};
-	
-	exports.default = AnimEvent;
-	module.exports = exports["default"];
+"use strict";
 
-/***/ }
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+/*
+ * AnimEvent
+ * https://github.com/anseki/anim-event
+ *
+ * Copyright (c) 2016 anseki
+ * Licensed under the MIT license.
+ */
+
+// *** Currently, this code except `export` is not ES2015. ***
+
+var MSPF = 1000 / 60,
+    // ms/frame (FPS: 60)
+KEEP_LOOP = 500,
+    requestAnim = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
+  setTimeout(callback, MSPF);
+},
+    cancelAnim = window.cancelAnimationFrame || window.mozCancelAnimationFrame || window.webkitCancelAnimationFrame || window.msCancelAnimationFrame || function (requestID) {
+  clearTimeout(requestID);
+},
+
+
+/**
+ * @typedef {Object} task
+ * @property {Event} event
+ * @property {function} listener
+ */
+
+/** @type {task[]} */
+tasks = [],
+    requestID,
+    lastFrameTime = Date.now();
+
+function step() {
+  var called, next;
+
+  if (requestID) {
+    cancelAnim.call(window, requestID);
+    requestID = null;
+  }
+
+  tasks.forEach(function (task) {
+    if (task.event) {
+      task.listener(task.event);
+      task.event = null;
+      called = true;
+    }
+  });
+
+  if (called) {
+    lastFrameTime = Date.now();
+    next = true;
+  } else if (Date.now() - lastFrameTime < KEEP_LOOP) {
+    // Go on for a while
+    next = true;
+  }
+  if (next) {
+    requestID = requestAnim.call(window, step);
+  }
+}
+
+function indexOfTasks(listener) {
+  var index = -1;
+  tasks.some(function (task, i) {
+    if (task.listener === listener) {
+      index = i;
+      return true;
+    }
+    return false;
+  });
+  return index;
+}
+
+var AnimEvent = {
+  /**
+   * @param {function} listener - An event listener.
+   * @returns {(function|null)} - A wrapped event listener.
+   */
+  add: function add(listener) {
+    var task;
+    if (indexOfTasks(listener) === -1) {
+      tasks.push(task = { listener: listener });
+      return function (event) {
+        task.event = event;
+        if (!requestID) {
+          step();
+        }
+      };
+    } else {
+      return null;
+    }
+  },
+
+  remove: function remove(listener) {
+    var iRemove;
+    if ((iRemove = indexOfTasks(listener)) > -1) {
+      tasks.splice(iRemove, 1);
+      if (!tasks.length && requestID) {
+        cancelAnim.call(window, requestID);
+        requestID = null;
+      }
+    }
+  }
+};
+
+exports.default = AnimEvent;
+module.exports = exports["default"];
+
+/***/ })
 /******/ ]);
 //# sourceMappingURL=anim-event.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,45 @@
+/* eslint-env node, es6 */
+
 'use strict';
 
 const webpack = require('webpack'),
   path = require('path'),
-  buildMode = require('yargs').argv.mode === 'build',
+  BUILD = process.env.NODE_ENV === 'production',
   PKG = require('./package'),
 
   BABEL_TARGET_PACKAGES = [
-  ].map(packageName => path.resolve(__dirname, `node_modules/${packageName}`) + path.sep);
+  ].map(packageName => path.resolve(__dirname, `node_modules/${packageName}`) + path.sep),
+
+  BABEL_PARAMS = {
+    presets: ['es2015'],
+    plugins: ['add-module-exports']
+  };
 
 module.exports = {
   entry: './src/anim-event.js',
   output: {
-    path: buildMode ? __dirname : path.join(__dirname, 'test'),
-    filename: buildMode ? 'anim-event.min.js' : 'anim-event.js',
+    path: BUILD ? __dirname : path.join(__dirname, 'test'),
+    filename: BUILD ? 'anim-event.min.js' : 'anim-event.js',
     library: 'AnimEvent',
     libraryTarget: 'var'
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
-        exclude: absPath =>
-          !BABEL_TARGET_PACKAGES.find(target => absPath.indexOf(target) === 0) &&
+        exclude: absPath => !BABEL_TARGET_PACKAGES.find(target => absPath.indexOf(target) === 0) &&
           absPath.split(path.sep).includes('node_modules'),
-        loader: 'babel',
-        query: {
-          presets: ['es2015'],
-          plugins: ['add-module-exports']
-        }
+        use: [{
+          loader: 'babel-loader',
+          options: BABEL_PARAMS
+        }]
       }
     ]
   },
-  devtool: buildMode ? null : 'source-map',
-  plugins: buildMode ? [
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.BannerPlugin(
-      `/*! ${PKG.title || PKG.name} v${PKG.version} (c) ${PKG.author.name} ${PKG.homepage} */`,
-      {raw: true})
-  ] : null
+  devtool: BUILD ? false : 'source-map',
+  plugins: BUILD ? [
+    new webpack.optimize.UglifyJsPlugin({compress: {warnings: true}}),
+    new webpack.BannerPlugin({raw: true,
+      banner: `/*! ${PKG.title || PKG.name} v${PKG.version} (c) ${PKG.author.name} ${PKG.homepage} */`})
+  ] : []
 };
